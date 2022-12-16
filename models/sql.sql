828f5220331fe7417fdc25416e6ddf1fc2265b77
Renato Dantas <renatodantas@braintech.pt>
2022-12-16 06:25:30 +0000

    v2.1

--- models/sql.sql
+++ models/sql.sql
@@ -13,8 +13,9 @@ CREATE TABLE setr.users(
 	user_role		INT,
 	username		VARCHAR(50),
 	password		TEXT,
+	name 				VARCHAR(50),
 	tag					VARCHAR(50),
-	schedule		VARCHAR(50),
+	schedule		VARCHAR(7),
 	CONSTRAINT users_pk PRIMARY KEY(id),
 	CONSTRAINT users_uk UNIQUE(username)
 );
@@ -28,20 +29,26 @@ CREATE TABLE setr.clocks(
 );
 
 CREATE TABLE setr.rooms(
-id 			SERIAL,
-name 		VARCHAR(50),
+	id 			SERIAL,
+	name 		VARCHAR(50),
 CONSTRAINT rooms_pk PRIMARY KEY(id)
 );
 
 CREATE TABLE setr.bookings(
 	id 						SERIAL,
-	room					INT,
 	author				INT,
-	datetime 			TIMESTAMPTZ(0),
-	duration 			INT,
+	room					INT,
+	start 				TIMESTAMPTZ(0),
+	final					TIMESTAMPTZ(0),
 	description 	TEXT,
-	auth 					BOOLEAN,
+	validated 		BOOLEAN 					DEFAULT false,
 	CONSTRAINT bookings_pk PRIMARY KEY(id)
+);
+
+CREATE TABLE setr.areas(
+	id 			SERIAL,
+	name 		VARCHAR(50),
+CONSTRAINT areas_pk PRIMARY KEY(id)
 );
 
 --- Cria Relações
@@ -50,25 +57,45 @@ ALTER TABLE setr.clocks ADD CONSTRAINT user_clock_fk FOREIGN KEY(id_user) REFERE
 ALTER TABLE setr.bookings ADD CONSTRAINT author_fk FOREIGN KEY(author) REFERENCES setr.users(id);
 ALTER TABLE setr.bookings ADD CONSTRAINT room_fk FOREIGN KEY(room) REFERENCES setr.rooms(id);
 
+--- Cria Tabelas Associativas
+CREATE TABLE setr.users_areas(
+	id_user 		INT,
+	id_area 		INT,
+CONSTRAINT users_areas_pk PRIMARY KEY(id_user, id_area)
+);
+
+--- Cria Relações
+ALTER TABLE setr.users_areas ADD CONSTRAINT user_area_fk FOREIGN KEY(id_user) REFERENCES setr.users(id);
+ALTER TABLE setr.users_areas ADD CONSTRAINT area_user_fk FOREIGN KEY(id_area) REFERENCES setr.areas(id);
+
+
 --- Insert Data
 INSERT INTO setr.user_roles(name) VALUES 
 ('Administrador'),
 ('Supervisor'),
 ('Funcionário');
 
-INSERT INTO setr.users(user_role, username, password, tag, schedule) VALUES 
-(1, 'administrador', 'password', 'eb:71:77:bb', '1111100'),
-(2, 'supervisor', 'password', 'eb:71:77:bu', '1111100'),
-(3, 'funcionario1', 'password', 'eb:71:77:bc', '1010111'),
-(3, 'funcionario2', 'password', 'eb:71:77:bg', '1110011'),
-(3, 'funcionario3', 'password', 'eb:71:77:bt', '0011111');
+INSERT INTO setr.users(user_role, username, password, name, tag, schedule) VALUES 
+(1, 'administrador', '$2b$10$wYHL4eiJoqED0IYZIqr3kuz72P6IrMlsUPKs6fn6K/RdKGsh.LDC6', 'Administrador', 'eb:71:77:bb', '0111110'),
+(2, 'supervisor', '$2b$10$ZBCVPchyqBf/FdUer.tDFOmU8fkhp8F1apHoTPFD0taxSIYxjjub.', 'Supervisor', 'eb:71:77:bu', '0111110'),
+(3, 'funcionario1', '$2b$10$Jl1cqCLJt9SAHS5M5/TtOeR3Xd3co.uiYXZ1Z6SrYBX0QQkQFaCti', 'Funcionário 1', 'eb:71:77:bc', '0111110'),
+(3, 'funcionario2', '$2b$10$MZejZGfN79G0l2uqdkNLq.qEcodPVmaGfmuZdFLg6BMro8MnSZvT.', 'Funcionário 2', 'eb:71:77:bg', '0011111'),
+(3, 'funcionario3', '$2b$10$U0ePB//72Sso4.WQUICtSOMByfTMjZ92SmifDGJpIn7e32tWf012O', 'Funcionário 3', 'eb:71:77:bt', '1111110');
 
 INSERT INTO setr.rooms(name) VALUES 
 ('Sala de Reuniões 1'),
 ('Sala de Reuniões 2'),
 ('Sala de Reuniões 3');
 
-INSERT INTO setr.bookings(room, author, datetime, duration, description, auth) VALUES 
-(1, 1, '2022-12-14T09:00:00.000Z', 60, 'Reuniao com XPTO 1, sala 1', 'true'),
-(1, 1, '2022-12-14T11:00:00.000Z', 30, 'Reuniao com XPTO 2, sala 1', 'true'),
-(2, 1, '2022-12-14T11:00:00.000Z', 45, 'Reuniao com XPTO 1, sala 2', 'false');
+INSERT INTO setr.bookings(room, author, start, final, description, validated) VALUES 
+(1, 1, '2022-12-14T09:00:00.000Z', '2022-12-14T10:00:00.000Z', 'Reuniao com XPTO 1, sala 1', 'true'),
+(1, 1, '2022-12-14T09:00:00.000Z', '2022-12-20T10:00:00.000Z', 'Reuniao com XPTO 1, sala 1', 'true'),
+(1, 1, '2022-12-14T11:00:00.000Z', '2022-12-14T11:30:00.000Z', 'Reuniao com XPTO 2, sala 1', 'true'),
+(2, 1, '2022-12-14T11:00:00.000Z', '2022-12-14T11:45:00.000Z', 'Reuniao com XPTO 1, sala 2', 'false');
+
+INSERT INTO setr.areas(name) VALUES 
+('Sala dos Servidores');
+
+INSERT INTO setr.users_areas(id_user, id_area) VALUES 
+(1, 1),
+(5, 1);
